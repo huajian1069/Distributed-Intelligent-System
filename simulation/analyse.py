82be694fe1c3065f2228bdc6710c8937b2415091
Darko Lukic <lukicdarkoo@gmail.com>
2019-12-12 13:43:53 +0100

    Add script to analyse fitness

---
+++ simulation/analyse.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np 
+import matplotlib.pyplot as plt
+
+FILENAME = 'webots_pso_1576111690.csv'
+
+results = np.genfromtxt(os.path.join(os.getenv("HOME"), FILENAME), delimiter=',')
+
+best_result_index = np.argmax(results[:, 6])
+best_result = np.max(results[:, 6])
+
+
+print(best_result_index, best_result)
+
+plt.plot(results[:, 6])
+plt.show()
